d7478965b99a3d80e0e9f2ad306c549132717a31
Emanuele <emanuele.pez1@gmail.com>
2024-03-04 15:28:19 +0100

    first commit

---
+++ D1.js
@@ -0,0 +1,68 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+console.log('Hello world!')
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+let replyOne =
+  'In Javascript there are different values we call "primitive", that can be assigned to a variable. We can observe strings, written between single or double quotes, which define the labels of a certain element and represent a textual type of data. Numbers are self-explainatory, they must be written without any quotation mark and can be used in various mathematical operations. Booleans define the absolute values of true/false. Undefined refers to variables not yet defined by any value, whilst type null is the explicitation of the absence of a value (different from the value 0)'
+console.log('1)', replyOne)
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+const myName = 'Emanuele'
+console.log('2)', myName)
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+let number1 = 10
+let number2 = 10
+console.log('3)', number1 + number2)
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+let number = 'x'
+console.log('4)', (x = 12))
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+//myName = 'Pezzato'
+console.log(
+  '5) This error is displayed because it is not allowed to change the value of a constant variable'
+)
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+let number3 = '4'
+console.log('6)', number3 - x)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+let name1 = 'john'
+let name2 = 'John'
+console.log('7)', name1 === name2)
+console.log('7 extra)', name1 !== name2)
